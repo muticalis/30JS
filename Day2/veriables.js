2a32c7a66fd8a3bc18845423874f98ae9f28d5c2
Mustafa Çalış <muticalis@gmail.com>
2023-02-20 19:51:24 +0300

    primitive data types

--- Day2/veriables.js
+++ Day2/veriables.js
@@ -13,5 +13,19 @@ Objects ( Nesneler )
 Arrays ( Diziler )
 Şimdi ilkel ve ilkel olmayan veri türlerinin tam olarak ne anlama geldiğini görelim. İlkel veri türleri değişmez (değiştirilemez) veri türleridir. İlkel bir veri türü oluşturulduktan sonra onu değiştiremeyiz./*/
 
+//Primitive Data Types
+let numOne = 2
+let numTwo = 2
 
-let name = "Javascript"
+console.log(numOne == numTwo) //true
+
+let js = 'Javascript'
+let py = 'Python'
+
+console.log(js == py) //false 
+
+let bool1 = true
+let bool2 = false
+
+console.log(bool1 == bool2) //false
+
