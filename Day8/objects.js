42d25f7938f2e38a75076eca8523f1349b70129e
Mustafa Çalış <muticalis@gmail.com>
2023-02-27 15:17:20 +0300

    objects

--- Day8/objects.js
+++ Day8/objects.js
@@ -24,12 +24,39 @@ console.log(name,surnmame) //Muti Çaliş
 
 //OBJECTS
 
+const user = {
+    name: 'Muti',
+    surnmame: 'Çaliş',
+    isMarried: false,
+    age:21,
+    skills: [
+        'HTML',
+        'CSS',
+        'JAVASCRİPT',
+        'PYTHON'
+    ]
+}
+
+console.log(user.name) //Muti
 
 
+const person = {
+    name: 'Muti',
+    surnmame: 'Çaliş',
+    isMarried: false,
+    age:21,
+    skills: [
+        'HTML',
+        'CSS',
+        'JAVASCRİPT',
+        'PYTHON'
+    ],
+   getFullName: function() {
+        return `${this.name}${this.surnmame}`
+   }
+}
 
-
-
-
+console.log(person.getFullName()) //MutiÇaliş
 
 
 
