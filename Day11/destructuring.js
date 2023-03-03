518f169ff349fbc670e8799c7c6ef1e916fb9341
Mustafa Çalış <muticalis@gmail.com>
2023-03-03 13:58:16 +0300

    working on destructuring

---
+++ Day11/destructuring.js
@@ -0,0 +1,16 @@
+const numbers = [2,4,6]
+const [number1,number2,number3] = numbers
+
+console.log(number1) //2
+console.log(number2) //4
+console.log(number3) //6
+
+
+const stack = [
+    ['HTML','CSS','JAVASCRİPT'],
+    ['GOLANG','PYTHON','JAVA']
+]
+
+const [frontend,backend] = stack
+console.log(frontend) //['HTML', 'CSS', 'JAVASCRİPT']
+console.log(backend) // ['GOLANG','PYTHON','JAVA']
