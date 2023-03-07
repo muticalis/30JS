6227d7a165780312249f3ec03dcf5bc14cf53727
Mustafa Çalış <muticalis@gmail.com>
2023-03-07 19:59:14 +0300

    working on classes

--- Day15/classes.js
+++ Day15/classes.js
@@ -1,9 +1,14 @@
 class Person {
-constructor (firstName,lastName) {
+constructor (firstName,lastName,age,country,city) {
 this.firstName = firstName
 this.lastName = lastName
+this.age = age
+this.country = country
+this.city = city
 }
 }
 
-const person = new Person('John', 'Doe')
-console.log(person)
+const person1 = new Person('John', 'Doe',30,'USA')
+const person2 = new Person('Muti', 'Çalış',21,'Turkey')
+const person3 = new Person('Adar', 'Sönmez',22,'Portugal')
+console.log(person1,person2,person3)
