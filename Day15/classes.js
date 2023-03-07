7fe4bcaa9f7fc39aa7fac9a2559cf4de03384555
Mustafa Çalış <muticalis@gmail.com>
2023-03-07 20:10:43 +0300

    working on Classes

--- Day15/classes.js
+++ Day15/classes.js
@@ -5,10 +5,23 @@ this.lastName = lastName
 this.age = age
 this.country = country
 this.city = city
+this.skills = []
+this.score = 0
 }
+getFullName() {
+    return this.firstName + ' ' + this.lastName
 }
+get getSkills () {
+    return this.getSkills
+}
+get getScore() {
+    return this.getScore
+}
+} 
 
 const person1 = new Person('John', 'Doe',30,'USA')
 const person2 = new Person('Muti', 'Çalış',21,'Turkey')
 const person3 = new Person('Adar', 'Sönmez',22,'Portugal')
-console.log(person1,person2,person3)
+console.log(person1.skills,
+    person2,
+    person3)
