f0c7672be0d8e553ee39e9a4b3eb42e29f598088
Mustafa Çalış <muticalis@gmail.com>
2023-03-07 22:33:15 +0300

    classes is done

--- Day15/classes.js
+++ Day15/classes.js
@@ -12,16 +12,45 @@ getFullName() {
     return this.firstName + ' ' + this.lastName
 }
 get getSkills () {
-    return this.getSkills
+    return this.skills
 }
 get getScore() {
-    return this.getScore
+    return this.score
+}
+set setScore(score) {
+    this.score += score
+}
+set setSkill(skill) {
+    this.skills.push(skill)
 }
 } 
 
 const person1 = new Person('John', 'Doe',30,'USA')
-const person2 = new Person('Muti', 'Çalış',21,'Turkey')
-const person3 = new Person('Adar', 'Sönmez',22,'Portugal')
-console.log(person1.skills,
-    person2,
-    person3)
+const person2 = new Person('Muti', 'Çaliş',21,'Turkey')
+
+person1.setScore = 1
+person1.setSkill = 'HTML'
+person1.setSkill = 'CSS'
+person1.setSkill = 'JavaScript'
+
+person2.setScore = 1
+person2.setSkill = 'Planning'
+person2.setSkill = 'Managing'
+person2.setSkill = 'Organizing'
+
+console.log(person1.score)
+console.log(person2.score)
+
+console.log(person1.skills)
+console.log(person2.skills)
+
+
+class Student extends Person {
+    saySomething() {
+      console.log('I am a child of the person class')
+    }
+  }
+  
+  const s1 = new Student('Muti', 'Çaliş', 'Turkiye', 21, 'Istanbul')
+  console.log(s1)
+  console.log(s1.saySomething())
