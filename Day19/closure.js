e8aa3d476e8d70d59a573aa5d7f2c4023d8afec3
Mustafa Çalış <muticalis@gmail.com>
2023-03-08 20:42:36 +0300

    closure is done

---
+++ Day19/closure.js
@@ -0,0 +1,20 @@
+function outerFunction() {
+    let count = 0;
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
+}
+const innerFuncs = outerFunction()
+
+console.log(innerFuncs.plusOne) // 1
+console.log(innerFuncs.minusOne) // 0
