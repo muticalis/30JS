3bf2216075ab44bb2a34a8524ca6ff0146ecfbe0
Mustafa Çalış <muticalis@gmail.com>
2023-03-08 19:42:31 +0300

    webstorages is done

---
+++ Day17/webstorages.js
@@ -0,0 +1,24 @@
+/*/ 
+localStorage - to display the localStorage object
+localStorage.clear() - to remove everything in the local storage
+localStorage.setItem() - to store data in the localStorage. It takes a key and a value parameters.
+localStorage.getItem() - to display data stored in the localStorage. It takes a key as a parameter.
+localStorage.removeItem() - to remove stored item form a localStorage. It takes key as a parameter.
+localStorage.key() - to display a data stored in a localStorage. It takes index as a parameter.
+/*/
+
+
+console.log(
+    localStorage
+)
+
+localStorage.setItem('firstName', 'Muti') 
+console.log(localStorage)
+
+
+
+const skills = ['HTML', 'CSS', 'JS', 'React']
+//Skills array has to be stringified first to keep the format.
+const skillsJSON = JSON.stringify(skills, undefined, 4)
+localStorage.setItem('skills', skillsJSON)
+console.log(localStorage)
